e4e72385cef554f79e634f9fe0eb6e27499f4b0f
michael.wilson <michael.wilson@journalism.cuny.edu>
2016-10-14 18:09:43 -0400

    first line

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	// this is where your pseudocode and jquery goes!
+	// make a selection option for each row in your data set, i.e. make a dropdown menu
+	
+	
+	// activate semantic-ui
+
+
+
+
+});
